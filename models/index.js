0a67b7e502532baadbbc3d45b110941a80930bd2
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-07-11 20:19:02 -0400

    added handlebars templates and routes for creating users, managing login and creating posts

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment')
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+
+module.exports = { User, Post, Comment };
