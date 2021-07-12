7be7a122b1b85fbc477d6751e929c6470f990737
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-07-11 21:22:49 -0400

    added maxage factor to cookie to ensure expiration

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    httpOnly: true,
+    maxAge: 900000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
